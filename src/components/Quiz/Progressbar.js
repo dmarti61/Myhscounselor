983a477b66dc84435db505138899277f4be0af38
dmarti61 <dmarti61@gmail.com>
2025-07-11 10:46:57 -0500

    Progressbar.js

---
+++ src/components/Quiz/Progressbar.js
@@ -0,0 +1,17 @@
+// src/components/Quiz/ProgressBar.js
+import React from 'react';
+import './ProgressBar.css'; // style as you like
+
+const ProgressBar = ({ currentStep, totalSteps }) => {
+  const percent = Math.round((currentStep / totalSteps) * 100);
+
+  return (
+    <div className="progress-container">
+      <div className="progress-bar" style={{ width: `${percent}%` }}>
+        <span className="progress-label">{percent}%</span>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
