cd3dafe3d182fe2f88e459b0d003171577ecbfb4
dmarti61 <dmarti61@gmail.com>
2025-07-13 04:40:42 -0500

    Update builderchecklist.js

--- src/pages/personalitychecklistpages/builderchecklist.js
+++ src/pages/personalitychecklistpages/builderchecklist.js
@@ -1,8 +1,8 @@
-// BuilderChecklist.js
+// src/pages/personalitychecklistpages/builderchecklist.jsx
 import React from 'react';
-import TradeVsCollegeTable from '../../components/Checklist/TradeVsCollegeTable';
-import InterestScale from '../../components/Checklist/InterestScale';
-import StillNotSureButton from '../../components/Checklist/StillNotSureButton';
+import TradeVsCollegeTable from '../../components/checklist/tradevscollegetable';
+import InterestScale from '../../components/checklist/interestscale';
+import StillNotSureButton from '../../components/checklist/stillnotsurebutton';
 
 const BuilderChecklist = () => {
   return (
