e312ac6ad10d0c0d2fc615222b16f6e8c5714420
dmarti61 <dmarti61@gmail.com>
2025-07-17 01:40:18 -0500

    Update layout.jsx

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
+import { useLocation, Outlet } from 'react-router-dom';
 import Navbar from './navbar';
 import Footer from './footer';
-import { Outlet } from 'react-router-dom';
 
-const Layout = ({ children }) => {
+const Layout = () => {
   const { pathname } = useLocation();
-  // Hide navbar on home "/" and any "/quiz" routes
   const hideNavbar = pathname === '/' || pathname.startsWith('/quiz');
+
   return (
     <>
       {!hideNavbar && <Navbar />}
-      {children}
-    </>
-  );
-};
-const Layout = () => {
-  return (
-    <>
-      <Navbar />
       <div className="main-content">
         <Outlet />
       </div>
