5ce29ec918a9a0c370562830a64f5cd7397689c5
dmarti61 <dmarti61@gmail.com>
2025-07-17 01:44:38 -0500

    Update layout.jsx

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLocation, Outlet } from 'react-router-dom';
 import Navbar from './navbar';
-import Footer from './footer';
+//import Footer from './footer';
 
 const Layout = () => {
   const { pathname } = useLocation();
@@ -13,7 +13,7 @@ const Layout = () => {
       <div className="main-content">
         <Outlet />
       </div>
-      <Footer />
+    <Footer />
     </>
   );
 };
