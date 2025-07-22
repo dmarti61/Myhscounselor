23660527fb654b0f92f7808243b48ada92b2c705
dmarti61 <dmarti61@gmail.com>
2025-07-21 22:26:47 -0500

    Update contact.jsx

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,11 +1,21 @@
 <section className="contact-page">
   <h1>📬 Contact & Feedback</h1>
   <p>We’re building this for you—and we want to hear what’s working, what’s missing, and what you’d love to see next.</p>
-
-  <iframe src="https://your-typeform-link" width="100%" height="600px" frameBorder="0"></iframe>
-
+    <iframe
+      id="JotFormIFrame-252018497652058"
+      title="My HS Counselor Feedback"
+      onload="window.parent.scrollTo(0,0)"
+      allowtransparency="true"
+      allow="geolocation; microphone; camera; fullscreen; payment"
+      src="https://form.jotform.com/252018497652058"
+      frameborder="0"
+      style="min-width:100%;max-width:100%;height:539px;border:none;"
+      scrolling="no"
+    >
+    </iframe>
+    <script src='https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js'></script>
+    <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-252018497652058']", "https://form.jotform.com/")</script>
+    
   <div className="contact-options">
-    <p>Prefer email? Reach us at <a href="mailto:hello@myhscounselor.com">hello@myhscounselor.com</a></p>
-    <p>Want to partner or share your story? <a href="/outreach">Submit here</a></p>
   </div>
 </section>
