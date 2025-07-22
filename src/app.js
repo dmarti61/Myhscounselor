ec8720e3ced8aa4784d3469590e106e83abbd390
dmarti61 <dmarti61@gmail.com>
2025-07-21 22:29:08 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/layout';
 import Navbar from './components/navbar';
-
+import Contact from '.pages/contact';
 import Home from './pages/home';
 import NotSure from './pages/notsure';
 import BuilderChecklist from './pages/personalitychecklistpages/builderchecklist';
@@ -34,7 +34,7 @@ const App = () => (
 
         {/* resources */}
         <Route path="/college-guide" element={<CollegeGuide />} />
-
+        <Route path="/contact" element={<Contact />} />
         {/* catch-all */}
         <Route path="*" element={<NotFound />} />
       </Route>
