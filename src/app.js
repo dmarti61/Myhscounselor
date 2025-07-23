66fe2bea0f1b52697bd14781864f3ef778a7008f
dmarti61 <dmarti61@gmail.com>
2025-07-22 23:14:59 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout';
 import Navbar from './components/navbar';
 import Contact from './pages/contact';
@@ -8,10 +8,6 @@ import Careers from './pages/careers';
 import TalkTemplates from './pages/talktemplates';
 import Home from './pages/home';
 import NotSure from './pages/notsure';
-import BuilderChecklist from './pages/personalitychecklistpages/builderchecklist';
-import PlannerChecklist from './pages/personalitychecklistpages/plannerchecklist';
-import ConnectorChecklist from './pages/personalitychecklistpages/connectorchecklist';
-import ExplorerChecklist from './pages/personalitychecklistpages/explorerchecklist';
 import Onboarding from './pages/onboarding/onboarding';
 import CollegeGuide from './pages/collegeguide';
 import NotFound from './pages/notfound';
@@ -21,7 +17,6 @@ import ResultBadge from './components/quiz/resultbadge';
 const App = () => (
   <Router>
     <Routes>
-      {/* Wrap main routes in Layout */}
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/not-sure" element={<NotSure />} />
@@ -29,19 +24,14 @@ const App = () => (
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/results" element={<ResultBadge />} />
 
-        {/* personality-type checklists */}
-        <Route path="/builder" element={<BuilderChecklist />} />
-        <Route path="/planner" element={<PlannerChecklist />} />
-        <Route path="/connector" element={<ConnectorChecklist />} />
-        <Route path="/explorer" element={<ExplorerChecklist />} />
-
-        {/* resources */}
+        {/* Resources */}
         <Route path="/college-guide" element={<CollegeGuide />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
         <Route path="/direct-entry-careers" element={<Careers />} />
         <Route path="/talk-templates" element={<TalkTemplates />} />
-        {/* catch-all */}
+
+        {/* Catch-all */}
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
