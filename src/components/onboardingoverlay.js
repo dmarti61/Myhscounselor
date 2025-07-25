30ae11c22ccd9d521f2b968de4bd03676526e55c
dmarti61 <dmarti61@gmail.com>
2025-07-24 19:27:26 -0500

    Update onboardingoverlay.js

--- src/components/onboardingoverlay.js
+++ src/components/onboardingoverlay.js
@@ -8,11 +8,11 @@ const OnboardingOverlay = () => (
       <h2 id="onboarding-title" tabIndex="0">🧭 Not Sure What Path to Take?</h2>
       <div id="onboarding-desc">
         <p>
-          Whether you're thinking about college, trade school, or something in between—
-          this quiz helps you discover your strengths and match them to real options.
+          Welcome! Whether you're thinking about college, trade school, or something in between—
+          this platform helps you discover your strengths and match them to real options.
         </p>
         <p>
-          Answer 20 quick questions to find your personality type followed by your preferences and get a personalized checklist
+          Answer 20 quick questions to find your personality type followed by your 4 preferences and get a personalized checklist
           with timelines, resources, and next steps.
         </p>
       </div>
