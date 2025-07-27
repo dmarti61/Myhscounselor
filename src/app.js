852835eee50eef06b29cccff12866e1a5af505d0
dmarti61 <dmarti61@gmail.com>
2025-07-27 00:42:56 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout';
-import Navbar from './components/navbar';
+import Navbar from './components/navbar'; // This import is not directly used in Routes, but likely in Layout
 import Contact from './pages/contact';
 import Explore from './pages/explore';
 import About from './pages/about';
 import Careers from './pages/careers';
-import Military from '.pages/military';
-import Trade from '.pages/tradeschoolguide';
+import Military from './pages/military'; // Corrected path from '.pages/military'
+import Trade from './pages/tradeschoolguide'; // Corrected path from '.pages/tradeschoolguide'
 import TalkTemplates from './pages/talktemplates';
 import Home from './pages/home';
 import NotSure from './pages/notsure';
@@ -28,14 +28,18 @@ const App = () => (
         <Route path="/preferences" element={<PreferenceQuiz />} />
         <Route path="/results" element={<ResultBadge />} />
 
-        {/* Resources */}
+        {/* Explore My Path */}
         <Route path="/college-guide" element={<CollegeGuide />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
         <Route path="/military" element={<Military />} />
         <Route path="/direct-entry-careers" element={<Careers />} />
-        <Route path="/talk-templates" element={<TalkTemplates />} />
         <Route path="/trade" element={<Trade />} />
+
+        {/* Resources */}
+        <Route path="/talk-templates" element={<TalkTemplates />} />
+
+        {/* About Us */}
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
 
         {/* Catch-all */}
         <Route path="*" element={<NotFound />} />
