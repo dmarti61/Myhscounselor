c027adea94dbe820770ccc0ef2b663dfca0b1c1f
dmarti61 <dmarti61@gmail.com>
2025-07-31 12:23:30 -0500

    Update explore.jsx

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -1,7 +1,7 @@
 // src/pages/ExploreCareers.js
 
 import React from 'react';
-import styles from '../styles/explore.css';
+import styles from '../styles/explore.module.css';
 import { MBTI_MAP } from '../components/quiz/mbtimap';
 
 const groupCareersByPathway = () => {
