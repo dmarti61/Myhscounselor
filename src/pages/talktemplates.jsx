c1abbd493615247a414a109fd92bb085924724ac
dmarti61 <dmarti61@gmail.com>
2025-08-01 11:05:31 -0500

    Update talktemplates.jsx

--- src/pages/talktemplates.jsx
+++ src/pages/talktemplates.jsx
@@ -52,7 +52,7 @@ const TalkTemplates = () => {
         </p>
       </div>
 
-  <hr>  </hr>
+ <hr />
 
       <div className="template-section">
         <h2>🎓 College Recommendation</h2>
@@ -62,7 +62,7 @@ const TalkTemplates = () => {
         </p>
       </div>
       <p></p>
-      <hr></hr>
+     <hr />
       <div className="template-section">
         <h2>🔧 Trade School Reference</h2>
         <p>
@@ -71,7 +71,7 @@ const TalkTemplates = () => {
         </p>
       </div>
       <p></p>
-      <hr></hr>
+      <hr />
       <div className="template-section">
         <h2>💼 Job Reference</h2>
         <p>
